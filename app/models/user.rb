589aab63b897fba2bcef1d776f4cb9d9b13199f7
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-04-27 12:38:13 +0900

    UserModelのnameとemailのプレゼンスのチェックを行う.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   has_many :projects, through: :project_members
   has_many :rank_histories
   has_many :partner_costs
+
+  #attr_accessor :name, :email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
