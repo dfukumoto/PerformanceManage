927ed12b3482548ba3da01e6145ae038af17a1ac
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-04-27 17:18:50 +0900

    UserModelに自分が管理者，社員，パートナーのうちどれなのかを応えるメソッドを作る．サインイン後のページでパートナー以外には「プロジェクト」リンクを貼る．

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :authority, presence: true
 
   has_secure_password
 
@@ -22,6 +23,18 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def admin?
+    self.authority.to_i == 1 ? true : false
+  end
+
+  def staff?
+    self.authority.to_i == 2 ? true : fales
+  end
+
+  def partner?
+    self.authority.to_i == 3 ? true : false
   end
 
   private
