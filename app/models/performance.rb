9ce50b21b75b5e1f0a83bd5a28b8599b105e1b08
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-04-28 16:19:09 +0900

    PerformanceModelの作業開始，終了時刻，作業内容を必須項目にする．作業時刻の時間オブジェクトを整形して返すクラスメソッドを作成する．

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,4 +1,27 @@
 class Performance < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
+  validates :start_time,  presence: true
+  validates :end_time,    presence: true
+  validates :content,     presence: true
+
+
+
+  def self.create_date
+    date_array = (Date.today.prev_month..Date.today).to_a
+    date_array.map!{|date| date.to_s }
+  end
+
+  def self.create_time
+    time_range = Time.now.all_day
+    time = time_range.first
+    last = time_range.last
+    return_array = []
+    while time < last
+      return_array << time
+      time += 30.minutes
+    end
+    return_array.map!{|time| time.strftime("%H:%M").to_s}
+  end
 end
