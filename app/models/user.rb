c68caef717349a357f3013fad83b1fe8d0aecf0a
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-04-28 10:41:44 +0900

    リレーションの修正を行う．

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ActiveRecord::Base
 
   has_many :projects
   has_many :performances
+  has_many :project_members
   has_many :projects, through: :project_members
+  has_many :partner_costs
+  has_many :projects, through: :partner_costs
   has_many :rank_histories
   has_many :partner_costs
 
