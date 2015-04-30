6cecf52e8fe528f32d317d8ba470dc4534da0246
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-04-30 14:40:35 +0900

    稼働実績のPerformanceModelにUserModelのidと対応するuser_idカラムを追加する．

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428021036) do
+ActiveRecord::Schema.define(version: 20150430022032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150428021036) do
     t.boolean  "permission", default: false, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "project_groups", force: true do |t|
