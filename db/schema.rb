6de46d7254bca47e5ea354df5b925daacef93d49
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-04-30 15:55:59 +0900

    PerformanceModelにproject_idカラムを追加する．

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430022032) do
+ActiveRecord::Schema.define(version: 20150430065436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150430022032) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "project_id"
   end
 
   create_table "project_groups", force: true do |t|
