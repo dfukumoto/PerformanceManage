23cc7eaae339746fdf2fcb305095e9d675889d15
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-04-30 16:04:59 +0900

    ProjectModelにプロジェクト名を格納するnameカラムを追加する．

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430065436) do
+ActiveRecord::Schema.define(version: 20150430070229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20150430065436) do
     t.string   "project_code"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "rank_histories", force: true do |t|
