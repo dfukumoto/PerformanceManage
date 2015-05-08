1b14f6b469af5c2edb5519dc7c2c17caf4cfdff0
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-05-08 17:02:36 +0900

    稼働実績一覧関連のテストコードを書く．
    * 稼働実績一覧に自分の稼働実績のみが表示されているか確認する．
    * 稼働実績の詳細画面に稼働実績の内容が表示されているか確認する．
    * 他人が作成した稼働実績の詳細画面にアクセスしようとした時，リダイレクトされているか確認する．
    * 自身が作成した稼働実績の編集画面に自身がアクセスできるか確認する．
    * 他人が作成した稼働実績の編集画面にアクセスした時，リダイレクトされるか確認する．

--- spec/requests/performance_pages_spec.rb
+++ spec/requests/performance_pages_spec.rb
@@ -92,4 +92,54 @@ RSpec.describe "PerformancePages", type: :request do
       end
     end
   end
+
+  describe "when access my performances index page" do
+    let!(:other_user) { FactoryGirl.create(:partner) }
+    let!(:user_performance)        { user.performances.first }
+    let!(:other_user_performance)  { other_user.performances.first }
+    before { visit performances_path }
+
+    describe "should render my performances" do
+      it "with info_button of performance" do
+        expect(page).to have_link("詳細", href: performance_path(user_performance))
+      end
+      it "with change_button of performance" do
+        expect(page).to have_link("変更", href: edit_performance_path(user_performance))
+      end
+      it "with delete_button of performance" do
+        expect(page).to have_link("削除", href: performance_path(user_performance))
+      end
+      it "shouldnt render other_users performances" do
+        expect(page).not_to have_link("詳細", href: performance_path(other_user_performance))
+      end
+    end
+
+    describe "should render my performance info view" do
+      before { visit performance_path(user_performance) }
+
+      it { should have_content(user_performance.user.name) }
+      it { should have_content(user_performance.project.name) }
+      it { should have_content(user_performance.start_time) }
+      it { should have_content(user_performance.end_time) }
+      it { should have_content(user_performance.content) }
+      it { should_not have_link("承認", approve_performance_path(user_performance)) }
+
+      describe "incorrect access to other user performances" do
+        before { visit performance_path(other_user_performance) }
+        it { should have_selector("div.alert.alert-danger", "管理者以外は他ユーザの稼働実績は見れません") }
+      end
+    end
+    
+    describe "should render my performance change view" do
+      before { visit edit_performance_path(user_performance) }
+
+      it { should have_content(user_performance.content) }
+      it { should have_selector("input.btn.btn-primary", "稼働登録") }
+
+      describe "incorrect access to other user performance change view" do
+        before { visit edit_performance_path(other_user_performance) }
+        it { should have_selector("div.alert.alert-danger", "稼働実績作成者以外は変更できません．") }
+      end
+    end
+  end
 end
