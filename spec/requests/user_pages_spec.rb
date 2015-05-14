580bbdc366e0a3106e4834ba482f2a61ef2636ce
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-05-14 13:39:37 +0900

    ユーザページのテストコードを書く．

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "NewUserPages", type: :request do
   let!(:admin) { FactoryGirl.create(:admin) }
+  let!(:staff) { FactoryGirl.create(:staff) }
 
   subject{ page }
 
@@ -106,6 +107,44 @@ RSpec.describe "NewUserPages", type: :request do
           expect(page).to have_selector("div.alert.alert-danger", text: "ユーザの新規作成に失敗しました")
         end
       end
+    end
+  end
+
+  describe "user index" do
+    before { click_link "ユーザ一覧", href: users_path }
+    it { should have_selector("table", text: admin.name) }
+    it { should have_selector("table", text: staff.name) }
+    it { should have_link("変更", href: edit_user_path(admin)) }
+    it { should have_link("変更", href: edit_user_path(staff)) }
+  end
+
+  describe "edit user" do
+    before {
+      click_link "ユーザ一覧", href: users_path
+      click_link "変更", href: edit_user_path(staff)
+    }
+    context "invalid info" do
+      before do
+        fill_in "user_name",  with: ""
+        fill_in "user_email", with: ""
+        click_button "変更"
+      end
+      it { should have_selector("div.alert.alert-danger", text: "ユーザ情報の変更に失敗しました．") }
+      it { should have_title("ユーザ編集") }
+    end
+    context "valid info" do
+      before do
+        fill_in "user_name",  with: "ChangedName"
+        fill_in "user_email", with: "change@change.com"
+        select "パートナー",   from: "user_authority"
+        click_button "変更"
+        staff.reload
+      end
+      it { should have_selector("div.alert.alert-success",  text: "ユーザ情報の変更に成功しました．") }
+      it { expect(current_path).to eq users_path }
+      it { expect(staff.name).to eq "ChangedName" }
+      it { expect(staff.email).to eq "change@change.com" }
+      it { expect(staff.authority).to eq "パートナー" }
     end
   end
 end
